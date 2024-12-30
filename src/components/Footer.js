513eb6dba6d752adaa1ae2a4f38cd1de39e16c25
yk7772703 <yk7772703@gmail.com>
2024-12-29 22:24:12 -0800

    sa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Dulat Akan</h3>
+          <h3>Designed and Developed by Boston</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SB</h3>
